c91d75325b3bba29ff5ded34c4348f6918803fd0
Runedia <runedia@naver.com>
2025-05-30 17:57:52 +0900

    logger 오류 수정

--- process/4_menu/get_menu_detail.js
+++ process/4_menu/get_menu_detail.js
@@ -5,7 +5,7 @@ const cheerio = require("cheerio");
 const fs = require("fs-extra");
 const path = require("path");
 const pool = require("@root/utils/db");
-const logger = require("@root/utils/logger");
+const { logger } = require("@root/utils/logger");
 
 const BASE_URL = "https://www.hoseo.ac.kr";
 const DOWNLOAD_ROOT = path.resolve(process.cwd(), "download_menu");
